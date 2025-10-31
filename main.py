53f7ea6c29f201f6f6f7e216c2ec70919b93f328
saimnabeel <Saimnabeeliqbal@gmail.com>
2025-10-31 17:48:49 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+name="saim"
+age = 15
+is_he_a_student=True
+weight=25.5
+print(type(name))
+print("name :", name)
+
+print(type(is_he_a_student))
+print("is_he_a_student :", is_he_a_student)
+
+print(type(weight))
+print("weight :", weight)
+ 
+print(type(age))
+
+
